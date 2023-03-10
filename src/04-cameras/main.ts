568f9e4d659bc1d077384a17429f66f545a59feb
Athirujkaewseesuk <athirujkaewseesuk>
2023-03-10 23:59:57 +0700

    Cameras part

--- src/04-cameras/main.ts
+++ src/04-cameras/main.ts
@@ -7,7 +7,10 @@ import {
     MeshBasicMaterial,
     Mesh,
     // Clock,
+    // Clock,
 } from 'three';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // import GSAP
 // import gsap from 'gsap'
@@ -17,6 +20,30 @@ import { sizes } from '../default';
 
 // //? Library Test
 // console.log(THREE)
+
+// Cursor
+// const cursor = {
+//     x: 0,
+//     y: 0
+// }
+
+// let mouseDown:Boolean = false;
+
+// window.addEventListener('mousedown',() => {
+//     mouseDown = true;
+// })
+
+// window.addEventListener('mousemove',(e) => {
+//     if (mouseDown){
+//     cursor.x = e.clientX / sizes.width - (3.97),
+//     cursor.y = e.clientY / sizes.height - (0.56) 
+//     // console.log(cursor.x,cursor.y)
+// }
+// })
+
+// window.addEventListener('mouseup',() => {
+//     mouseDown = false;
+// })
 
 // Canvas
 const canvas = document.querySelector('.webgl4');
@@ -39,6 +66,11 @@ const camera = new PerspectiveCamera(75, sizes.width / sizes.height);
 
 camera.position.set(0, 0, 3)
 
+// Controls
+const controls = new OrbitControls(camera, canvas as HTMLElement);
+// controls.target;
+// controls.enableDamping = true;
+
 // AxesHelper
 const axesHelper = new AxesHelper(3);
 
@@ -51,4 +83,23 @@ const renderer = new WebGLRenderer({
 
 renderer.setSize(sizes.width, sizes.height);
 
-renderer.render(scene, camera);
+//! THREE Normal Way
+// const clock = new Clock()
+
+const tick = () => {
+    // const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    // camera.position.y = cursor.y
+    // camera.position.x = -cursor.x
+
+    controls.update
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
